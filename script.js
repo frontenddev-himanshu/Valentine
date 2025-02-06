4716db16fdd08f48819e42a7e253ae7bad789c5c
frontenddev-himanshu <rajh5343@gmail.com>
2025-02-06 22:53:45 +0530

    add

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function open() {
     document.querySelector(".purpose").style.opacity = 1;
+    document.querySelector(".purpose").style.zIndex = 1;
 }
-// setTimeout(open, 1000)
+setTimeout(open, 500)
 
 document.querySelector(".cancel").addEventListener("click", function() {
 
     document.querySelector(".purpose").style.opacity = 0;
     document.querySelector(".purpose").style.zIndex = -1;
+
+    document.querySelector(".say").style.opacity = 1;
 
 })
 
