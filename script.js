3b602a9b9f60bc3a75249bac5c7c0fca55e35d14
frontenddev-himanshu <rajh5343@gmail.com>
2025-02-06 21:56:26 +0530

    some add

---
+++ script.js
@@ -0,0 +1,15 @@
+function open() {
+    document.querySelector(".purpose").style.opacity = 1;
+}
+setTimeout(open, 1000)
+
+document.querySelector(".cancel").addEventListener("click", function() {
+
+    document.querySelector(".purpose").style.opacity = 0;
+    document.querySelector(".purpose").style.zIndex = -1;
+
+})
+
+
+
+
